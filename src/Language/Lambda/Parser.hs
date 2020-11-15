93c3a44a92cfe6f3f2e302fabb8b462aca8bc355
Naufal Alauddin Hilmi <naufal.alauddin@ui.ac.id>
2020-11-15 13:36:20 +0700

    add parser for multiplication (*)

--- src/Language/Lambda/Parser.hs
+++ src/Language/Lambda/Parser.hs
@@ -15,7 +15,7 @@ expr :: Parser (LambdaExpr String)
 expr = try app <|> term
 
 term :: Parser (LambdaExpr String)
-term = let' <|> abs <|> var <|> parens <|> numeral <|> plus
+term = let' <|> abs <|> var <|> parens <|> numeral <|> plus <|> mult
 
 var :: Parser (LambdaExpr String)
 var = Var <$> identifier
@@ -61,3 +61,7 @@ numeral = church <$> lexeme digit
 
 plus = plus' <$> lexeme (char '+')
     where plus' _ = Abs "m" (Abs "n" (Abs "f" (Abs "x" ( App (App (Var "m") (Var "f")) (App (App (Var "n") (Var "f")) (Var "x"))))))
+
+mult = mult' <$> lexeme (char '*')
+    where mult' _ = Abs "m" (Abs "n" (Abs "f" (Abs "x" (App (App (Var "m") (App (Var "n") (Var "f"))) (Var "x")))))
+
